d92b9d9e180fbf3fd281faeb0784deb472607d1e
chd7hc <drakechau1@gmail.com>
2023-03-17 14:40:13 +0700

    update: optimize code

--- app/src/main/java/com/example/glucometric1/takesample/AddSampleFragment.java
+++ app/src/main/java/com/example/glucometric1/takesample/AddSampleFragment.java
@@ -3,10 +3,8 @@ package com.example.glucometric1.takesample;
 import static android.content.ContentValues.TAG;
 
 import android.app.Activity;
-import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Vibrator;
 import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -51,7 +49,8 @@ import java.util.List;
  * create an instance of this fragment.
  */
 public class AddSampleFragment extends Fragment {
-    public static final ArrayList<String> listWavelengthLabels = new ArrayList<>(List.of("410", "435", "460", "485", "510", "535", "560", "585", "610", "645", "680", "705", "730", "760", "810", "860", "900", "940"));
+    public static final ArrayList<String> listWavelengthLabels = new ArrayList<>(
+            List.of("410", "435", "460", "485", "510", "535", "560", "585", "610", "645", "680", "705", "730", "760", "810", "860", "900", "940"));
     // TODO: Variables are defined by user
     private static String CSV_FILE_PATH;
     private static String CSV_FILE_NAME;
@@ -59,19 +58,15 @@ public class AddSampleFragment extends Fragment {
     private static final ArrayList<String> arrayList = new ArrayList<>();
     private static Button btnTakeSample, btnSave, btnRandom;
     private static EditText editTextWavelengthValues, editTextGlucoseValue, editTextNote;
+    private static EditText editTextHeight, editTextWeight, editTextAge;
     private static BarData barData;
     private static BarDataSet barDataSet;
     private static BarChart barchart;
     private static ProgressBar progressBarSavaData;
     private static ArrayList<BarEntry> barEntriesList;
-    private static Vibrator vibrator;
     private static InputMethodManager imm;
-    private static Spinner spinner;
-
-
-    public AddSampleFragment() {
-
-    }
+    private static Spinner spinnerSex;
+    private NotifyAffect notifyAffect;
 
     // TODO: Rename and change types and number of parameters
     public static AddSampleFragment newInstance(String param1, String param2) {
@@ -79,22 +74,7 @@ public class AddSampleFragment extends Fragment {
         return fragment;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        APP_SCRIPT_URL = getResources().getString(R.string.app_script_url);
-        CSV_FILE_NAME = getResources().getString(R.string.csv_file_name);
-        CSV_FILE_PATH = getActivity().getFilesDir() + "/" + CSV_FILE_NAME;
-        Log.i("CSV_FILE_PATH", CSV_FILE_PATH);
-
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_add_sample, container, false);
-
+    private void initComponent(View view) {
         // Bind component to layout xml by id && service variables
         {
             btnSave = view.findViewById(R.id.btnSave);
@@ -103,11 +83,13 @@ public class AddSampleFragment extends Fragment {
             editTextWavelengthValues = view.findViewById(R.id.editTextWavelengthValues);
             editTextNote = view.findViewById(R.id.editTextNote);
             editTextGlucoseValue = view.findViewById(R.id.editTextGlucoseValue);
+            editTextHeight = view.findViewById(R.id.editTextHeight);
+            editTextWeight = view.findViewById(R.id.editTextWeight);
+            editTextAge = view.findViewById(R.id.editTextAge);
             barchart = view.findViewById(R.id.barchart);
             progressBarSavaData = view.findViewById(R.id.progressBarSavaData);
-            spinner = view.findViewById(R.id.spinnerSex);
+            spinnerSex = view.findViewById(R.id.spinnerSex);
 
-            vibrator = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
             imm = (InputMethodManager) getActivity().getSystemService(Activity.INPUT_METHOD_SERVICE);
         }
 
@@ -128,69 +110,100 @@ public class AddSampleFragment extends Fragment {
             // Specify the layout to use when the list of choices appears
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             // Apply the adapter to the spinner
-            spinner.setAdapter(adapter);
+            spinnerSex.setAdapter(adapter);
         }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        APP_SCRIPT_URL = getResources().getString(R.string.app_script_url);
+        CSV_FILE_NAME = getResources().getString(R.string.csv_file_name);
+        CSV_FILE_PATH = getActivity().getFilesDir() + "/" + CSV_FILE_NAME;
+        Log.i("CSV_FILE_PATH", CSV_FILE_PATH);
+
+        notifyAffect = new NotifyAffect(getActivity());
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_add_sample, container, false);
+
+        initComponent(view);
 
         // TODO: Override functions by programmer
-        /*
-         * TakeSample signal to GlucoseDevice to get Wavelength values over BLE connection
-         * */
+        // TakeSample signal to GlucoseDevice to get Wavelength values over BLE connection
         btnTakeSample.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
             }
         });
+        // Save spectra wavelengths to local (CSV) and cloud (GoogleSheet)
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.i("btnSave", "Save");
                 boolean validGlucoseValue = editTextGlucoseValue.length() > 0;
                 //boolean validNote = editTextNote.length() > 0 ? true : false;
                 if (validGlucoseValue) {
-                    appendData2CSVFile(CSV_FILE_PATH, arrayList);
-                    appendData2Database();
-
-                    editTextGlucoseValue.setHint(editTextGlucoseValue.getText());
-                    editTextGlucoseValue.setText("");
-
+                    ArrayList<String> data4Saving = processData4Saving();
+                    appendData2CSVFile(CSV_FILE_PATH, data4Saving);
+                    appendData2Database(data4Saving);
+                    // Hide softKeyBoard
                     imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
                 } else {
-                    vibrator.vibrate(200);
-                    Toast.makeText(getActivity(), "Glucose value is empty", Toast.LENGTH_LONG).show();
+                    notifyAffect.makeFailed("Glucose value is empty");
                 }
             }
         });
+        // Simulate data (for only debugging mode)
         btnRandom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.i("Debug", "Random");
-
-                // Bar Chart processing
-                barEntriesList = new ArrayList<>();
-                barchart.clear();
-                editTextWavelengthValues.setText(null);
-                String textStringValues = "";
-
-                arrayList.clear();
-                for (int i = 0; i < 18; i++) {
-                    double rand = Math.random() * 10000 + 0;
-                    arrayList.add(String.valueOf(rand));
-                    textStringValues += String.format("<b>%s:</b>%.0f   ", listWavelengthLabels.get(i), rand);
-                    barEntriesList.add(new BarEntry(i, (float) rand));
-                }
-
-                editTextWavelengthValues.setText(Html.fromHtml(textStringValues, Html.FROM_HTML_MODE_COMPACT));
-
-                barDataSet = new BarDataSet(barEntriesList, "Wavelength");
-                barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
-                barDataSet.setValueTextColor(Color.BLACK);
-                barData = new BarData(barDataSet);
-                barchart.animateY(500);
-                barchart.setData(barData);
+                Log.i("btnRandom", "Random");
+                simulateDataRandom();
             }
         });
 
+        // View start visualize affectively
+        simulateDataRandom();
+
         return view;
+    }
+
+    private void simulateDataRandom() {
+        // Reset barchart
+        resetBarChart();
+        editTextWavelengthValues.setText(null);
+        String textStringValues = "";
+        arrayList.clear();
+
+        for (int i = 0; i < 18; i++) {
+            double rand = Math.random() * 10000 + 0;
+            arrayList.add(String.valueOf(rand));
+            textStringValues += String.format("<b>%s:</b>%.0f   ", listWavelengthLabels.get(i), rand);
+            barEntriesList.add(new BarEntry(i, (float) rand));
+        }
+
+        editTextWavelengthValues.setText(Html.fromHtml(textStringValues, Html.FROM_HTML_MODE_COMPACT));
+
+        handleBarChar(barEntriesList, "Wavelength");
+    }
+
+    private void resetBarChart() {
+        barEntriesList = new ArrayList<>();
+        barchart.clear();
+    }
+
+    private void handleBarChar(ArrayList entriesList, String label) {
+        barDataSet = new BarDataSet(entriesList, label);
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        barDataSet.setValueTextColor(Color.BLACK);
+        barData = new BarData(barDataSet);
+        barchart.animateY(500);
+        barchart.setData(barData);
     }
 
     private void setEnableComponent(boolean b) {
@@ -198,6 +211,18 @@ public class AddSampleFragment extends Fragment {
         btnSave.setEnabled(b);
         btnRandom.setEnabled(b);
         editTextWavelengthValues.setEnabled(b);
+    }
+
+    public ArrayList<String> processData4Saving() {
+        // Format: wavelengths[18],age,height,weight,sex,note
+        // Total field: 24
+        ArrayList<String> dataList = new ArrayList<>(arrayList);
+        dataList.add(editTextAge.getText().toString());
+        dataList.add(editTextHeight.getText().toString());
+        dataList.add(editTextWeight.getText().toString());
+        dataList.add(spinnerSex.getSelectedItem().toString());
+        Log.i("processData4Saving", Arrays.toString(arrayList.toArray()));
+        return dataList;
     }
 
     private void volleyHTTPRequest(int requestMethod, String query) {
@@ -210,8 +235,8 @@ public class AddSampleFragment extends Fragment {
             @Override
             public void onResponse(String response) {
                 Log.i("onResponse", response);
-                Toast.makeText(getActivity(), "Data was written to DB", Toast.LENGTH_SHORT).show();
 
+                notifyAffect.makeSuccess("Data was written to DB");
                 progressBarSavaData.setVisibility(View.GONE);
                 setEnableComponent(true);
             }
@@ -219,8 +244,8 @@ public class AddSampleFragment extends Fragment {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Log.i("onResponse", error.toString());
-                Toast.makeText(getActivity(), error.toString(), Toast.LENGTH_SHORT).show();
 
+                notifyAffect.makeFailed(error.toString());
                 progressBarSavaData.setVisibility(View.GONE);
                 setEnableComponent(true);
             }
@@ -230,13 +255,13 @@ public class AddSampleFragment extends Fragment {
         queue.add(stringRequest);
     }
 
-    private void appendData2Database() {
+    private void appendData2Database(ArrayList<String> arrayData) {
         /*
          * -> Format data as string: value1|value2|...|value18|glucoseValue|note
          * <- At AppScript: decode by using String.split(',')
          */
         String dataAsString = "";
-        for (String x : arrayList) {
+        for (String x : arrayData) {
             dataAsString += x + "|";
         }
         dataAsString += editTextGlucoseValue.getText().toString();
@@ -245,7 +270,6 @@ public class AddSampleFragment extends Fragment {
         Log.i("dataAsString", dataAsString);
         volleyHTTPRequest(Request.Method.GET, "itemName=" + dataAsString);
     }
-
     public boolean appendData2CSVFile(String outputPath, ArrayList<String> arrayList) {
         // Source base on: https://www.geeksforgeeks.org/writing-a-csv-file-in-java-using-opencsv/
         File file = new File(outputPath);
@@ -254,6 +278,7 @@ public class AddSampleFragment extends Fragment {
             FileWriter outfile = new FileWriter(file, true);
             CSVWriter writer = new CSVWriter(outfile);
 
+            // Convert ArrayList to String[]
             String[] dataArray = arrayList.toArray(new String[arrayList.size()]);
             writer.writeNext(dataArray);
 
---
+++ app/src/main/java/com/example/glucometric1/takesample/NotifyAffect.java
@@ -0,0 +1,48 @@
+package com.example.glucometric1.takesample;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.fragment.app.FragmentActivity;
+
+public class NotifyAffect {
+    private FragmentActivity activity;
+    private Vibrator vibrator;
+
+    NotifyAffect(FragmentActivity activity) {
+        if (activity == null) {
+            Log.e("NotifyAffect", "Null activity");
+            return;
+        }
+        this.activity = activity;
+        vibrator = (Vibrator) activity.getSystemService(Context.VIBRATOR_SERVICE);
+    }
+
+    public void makeVibrate(long millis) {
+        // this effect creates the vibration of default amplitude for *millis* second
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            VibrationEffect vibrationEffect1 = VibrationEffect.createOneShot(millis,
+                    VibrationEffect.DEFAULT_AMPLITUDE);
+            vibrator.cancel();
+            vibrator.vibrate(vibrationEffect1);
+        }
+    }
+
+    public void makeSuccess(String noty) {
+        makeVibrate(100);
+        Toast.makeText(this.activity,
+                noty,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void makeFailed(String noty) {
+        makeVibrate(200);
+        Toast.makeText(this.activity,
+                noty,
+                Toast.LENGTH_SHORT).show();
+    }
+}
